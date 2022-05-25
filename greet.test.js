a2f8740cc1d3ad25a80a41b0579cee4206d50756
Yanga <yangasiqiki@gmail.com>
2022-05-25 11:16:03 +0200

    first commit

---
+++ greet.test.js
@@ -0,0 +1,16 @@
+describe('', function(){
+
+	it(' ', function() {
+		// test the scenario here...
+
+		assert.equal('',);
+	});
+
+
+	it(' ', function() {
+		// test the scenario here...
+		assert.equal('',);
+	});
+
+    
+});
