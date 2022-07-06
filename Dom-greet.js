072ddf55b73b44b2221dbe9f57e337dff12d51cf
Yanga <yangasiqiki@gmail.com>
2022-07-06 16:31:05 +0200

    changes

--- Dom-greet.js
+++ Dom-greet.js
@@ -2,21 +2,23 @@ import Greet from "./main.js";
 const greet = Greet()
 
 const names = document.querySelector("name");
-const isixhosa= document.querySelector("language");
-const inglish= document.querySelector("language");
-const afrikaans= document.querySelector("language");
+const isixhosa = document.querySelector("language");
+const inglish = document.querySelector("language");
+const afrikaans = document.querySelector("language");
 const buttonElement = document.querySelector(".button");
 const clearElement = document.querySelector(".clear");
 const inputName = document.querySelector('.inputName');
 const showElement = document.querySelector('.showElement');
 const counter = document.querySelector('.counter');
 const errorMessage = document.querySelector('.error-message')
+
 let storeNames = [];
-if(localStorage.getItem('names')){
+if (localStorage.getItem('names')) {
     storeNames = JSON.parse(localStorage.getItem('names'))
-}else{
-    storeNames = localStorage.setItem('names', JSON.stringify(storeNames))
 }
+// else{
+// storeNames = localStorage.setItem('names', JSON.stringify(storeNames))
+// }
 
 counter.innerHTML = storeNames.length
 
@@ -26,33 +28,38 @@ function greetings() {
     let result = language.value ? language.value : '';
     var storedWord = inputName.value;
 
-        if(storedWord !== ""){
-            showElement.innerHTML = ""
-        } 
-        if(result !== null){
-            if(result === "isixhosa"){
-                showElement.innerHTML = "Molo, " + storedWord;
+    if (storedWord !== "") {
+        showElement.innerHTML = ""
+
+        if (result !== null) {
+            if(storeNames.includes(storedWord)) {
+                showElement.innerHTML = ('the name duplicates');
+                result;
+            } else {
+                if (result === "isixhosa") {
+                    showElement.innerHTML = "Molo, " + storedWord;
+                }
+                else if (result === "english") {
+                    showElement.innerHTML = "Hello, " + storedWord;
+                }
+                else if (result === "sesotho") {
+                    showElement.innerHTML = "Dumela, " + storedWord;
+                }
+                storeNames.push(storedWord);
+                localStorage.setItem('names', JSON.stringify(storeNames))
             }
-            else if(result === "english"){
-                showElement.innerHTML = "Hello, " + storedWord;
-            }
-            else if(result === "sesotho"){
-            showElement.innerHTML = "Dumela, " + storedWord;
-            }
-            storeNames.push(storedWord);
-            localStorage.setItem('names', JSON.stringify(storeNames))
-            
+
             counter.innerHTML = storeNames.length;
         }
 
+    }
+
+    
 }
+clearElement.addEventListener('click', () => {
+    localStorage.removeItem('names');
+    location.reload()
 
+})
 
-
-
-
-clearElement.addEventListener('click', () => localStorage.removeItem('names'));
- 
 buttonElement.addEventListener('click', greetings);
-
-
--- main.js
+++ main.js
@@ -10,10 +10,24 @@ function Greet() {
     function storedName (){
         
     }
+    function errorMessage(name,language){
+        if(name =''&& language === null)
+        return; errorMessage('Please enter your name and language!')
+    }
+
+    function errorMessage(name,language){
+        if(language === null)
+        return; errorMessage('Please select language!')
+    }
+    function errorMessage(name,language){
+        if(name!=='')
+        return; errorMessage('Please enter your name!')
+    }
     return{
         setName,
         getName,
-        storedName
+        storedName,
+        errorMessage
     }
 }
 
