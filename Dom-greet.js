b5878323802474ad04b71138fa291443b56a8d84
Yanga <yangasiqiki@gmail.com>
2022-07-07 15:59:17 +0200

    small changes

--- Dom-greet.js
+++ Dom-greet.js
@@ -16,7 +16,14 @@ let storeNames = [];
 if (localStorage.getItem('names')) {
     storeNames = JSON.parse(localStorage.getItem('names'))
 }
-
+function regex(rejectnames) {
+    let letters = /\d/gi
+    if (letters.test(rejectnames)) {
+        return 'please use alphabets';
+    } else {
+        return true;
+    }
+}
 
 counter.innerHTML = storeNames.length
 
@@ -25,14 +32,20 @@ function greetings() {
     let language = document.querySelector(".radio-input:checked");
     let result = language != undefined ? language.value : null;
     var storedWord = inputName.value.toLowerCase();
+    console.log(regex(storedWord))
+    regex(storedWord)
 
     if (names.value == '' && language == undefined) {
-        errorMessage.innerHTML = ('Please enter name and language')
+        errorMessage.innerHTML = 'Please enter name and language'
+
+    }else if(/\d/g.test(storedWord)){
+        errorMessage.innerHTML ='Please use Alphabets'
+
     } else if (language == undefined) {
-        errorMessage.innerHTML = ('Please select language!');
-        
-    }else if(names.value === ''){
-        errorMessage.innerHTML = ('Please enter name ')
+        errorMessage.innerHTML = 'Please select language!';
+
+    } else if (names.value === '') {
+        errorMessage.innerHTML = 'Please enter name '
     }
 
     if (storedWord !== "") {
@@ -40,8 +53,8 @@ function greetings() {
 
         if (result !== null) {
             if (storeNames.includes(storedWord)) {
-                showElement.innerHTML = ('the name duplicates');
-            
+                showElement.innerHTML = 'the name duplicates'
+
             } else {
                 if (result === "isixhosa") {
                     showElement.innerHTML = "Molo, " + storedWord;
